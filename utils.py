b72d1f4f88453ea3ca85d26c0f1d6d1bd3610e3c
xxojaxx <j.kucharska2003@gmail.com>
2025-05-13 14:11:19 +0200

    NewCommit

---
+++ utils.py
@@ -0,0 +1,9 @@
+# przykladowe funkcje utils . py
+def add ( a : int , b : int ) -> int :
+    return a + b
+def subtract ( a : int , b : int ) -> int :
+    return a - b
+def multiply ( a : int , b : int ) -> int :
+    return a * b
+def divide ( a : int , b : int ) -> float :
+    return a / b
